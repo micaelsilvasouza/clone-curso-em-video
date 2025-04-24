133d6adea041a55edbab7a6cca9a4c6cd0d33c34
Filipe520 <filipealves520@gmail.com>
2025-04-24 18:20:42 -0300

    add nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 export default function Home() {
-  return 
-  (<div>
+  return;
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li>HOme</li>
+          <li>Contatos</li>
+        </ul>
+      </nav>
+    </header>
     <h1>algo demais</h1>
-  </div>);
+  </div>;
 }
