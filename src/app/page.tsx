dde4706c3f2864f57f431610a979717663e71ef8
Filipe520 <filipealves520@gmail.com>
2025-04-24 17:02:35 -0300

    lorem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,13 @@ export default function Home() {
     <div>
       <h1>Olá, REACT</h1>
       <h2>oiu</h2>
+
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem dolore
+        corrupti est soluta consequuntur error nam, dicta mollitia, quae
+        inventore eos at quisquam blanditiis ut libero rem. Explicabo, sequi
+        nihil.
+      </p>
     </div>
   );
 }
