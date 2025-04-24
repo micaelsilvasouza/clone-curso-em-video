2bc79c89bd7a6b8d3bf3271a75e12426b5f450f8
Filipe520 <filipealves520@gmail.com>
2025-04-24 17:59:03 -0300

    deletando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>Olá, REACT</h1>
-      <h2>oiu</h2>
-
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem dolore
-        corrupti est soluta consequuntur error nam, dicta mollitia, quae
-        inventore eos at quisquam blanditiis ut libero rem. Explicabo, sequi
-        nihil.
-      </p>
-
-      <form action="">
-        <input type="text" name="" id="" />
-        <button>Qualquer coisa</button>
-
-        <h1>Hey</h1>
-      </form>
-    </div>
-  );
+  return <div></div>;
 }
