5816245f6bd4bf92a913dad779a946e84740cb4e
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-04-24 16:23:45 -0300

    Adicionando tag main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,10 @@ export default function Home() {
   return 
   (<div>
     <h1>algo demais</h1>
+    <main>
+      <form action="">
+        <input type="text" value="algo muito demais"/>
+      </form>
+    </main>
   </div>);
 }
