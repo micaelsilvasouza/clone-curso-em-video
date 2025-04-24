92b224ea24d363994802ed3e1ada31823f87f3a1
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-04-24 16:08:33 -0300

    Alterando o body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return 
-  (<div>
+  <div>
     <h1>algo demais</h1>
     <main>
       <form action="">
         <input type="text" value="algo muito demais"/>
       </form>
     </main>
-  </div>);
+  </div>
 }
