a40c7b33dda25984d0b1a8ecadba0dcb9d22eb1e
Filipe520 <filipealves520@gmail.com>
2025-04-25 21:41:44 -0300

    Resolvendo problema function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
 export default function Home() {
-  return;
-  <div>
-    <header>
-      <nav>
-        <ul>
-          <li>HOme</li>
-          <li>Contatos</li>
-        </ul>
-      </nav>
-    </header>
-    <h1>algo demais</h1>
-    <main>
-      <form action="">
-        <input type="text" value="algo muito demais" />
-      </form>
-    </main>
-  </div>;
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>HOme</li>
+            <li>Contatos</li>
+          </ul>
+        </nav>
+      </header>
+      <h1>algo demais</h1>
+      <main>
+        <form action="">
+          <input type="text" value="algo muito demais" />
+        </form>
+      </main>
+    </div>
+  );
 }
