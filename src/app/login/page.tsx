44306f5753c022a8877bbf02d90b8854771c39d6
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-04-26 17:58:54 -0300

    Add form login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,14 @@
 export default function Login() {
   return (
     <div>
-      <h1>Chegou aqui no Login</h1>
+      <main>
+        <section className={styles.conteiner_form}>
+          <div style={{width: "200px"}}>
+            <Img img="logoBlue.png" alt="logo azul do curso em video"/>
+          </div>
+          <FormLogin />
+        </section>
+      </main>
     </div>
   );
 }
