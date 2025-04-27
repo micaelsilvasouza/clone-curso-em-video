06c8193e9c093c455626aa033d8259cc961d6693
Filipe520 <filipealves520@gmail.com>
2025-04-26 21:54:07 -0300

    Mexendo nas rotas

--- src/app/cadastre-se/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Cadastre_se() {
-  return (
-    <div>
-      <h1>Chegou aqui no Cadastre-se</h1>
-    </div>
-  );
-}
---
+++ src/app/minha-conta/senha-perdida/page.tsx
@@ -0,0 +1,14 @@
+export default function LostPassword() {
+  return (
+    <div>
+      <h1>Senha perdida</h1>
+
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
+        repellendus repellat adipisci dolores deleniti labore non qui, voluptate
+        cum, temporibus harum libero fugit laboriosam ex ipsum, exercitationem
+        sit blanditiis quis.
+      </p>
+    </div>
+  );
+}
