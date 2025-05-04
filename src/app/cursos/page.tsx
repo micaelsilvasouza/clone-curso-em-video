44c95d21dc3844b8c9d2af3809d799a80529a08a
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-05-04 12:41:49 -0300

    Alterando e usando os componentes BarraPaginaCima e BlocoAcesso

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -1,5 +1,5 @@
+import BannerPaginaCima from "../components/BannerPaginaCima";
 import Img from "../components/layout/img";
-//import Image from "next/image";
 import Link from "next/link";
 
 export default async function Cursos() {
@@ -10,34 +10,7 @@ export default async function Cursos() {
 
   return (
     <section className="text-black bg-white ml-5 mr-5 max-w-7xl">
-      <section className="inforCurso bg-[url(/image/BannerPaginaCima.jpg)] bg-center bg-current">
-        <div className="py-5">
-          <h1 className="text-5xl font-bold py-2">Cursos</h1>
-        </div>
-        <div className="py-5">
-          <nav>
-            <ul className="flex gap-2">
-              <li>
-                <Link href="/" className="text-blue-500">
-                  Início
-                </Link>
-              </li>
-              <div>&rarr;</div>
-              <li>
-                <Link href="/cursos" className="text-gray-600">
-                  Cursos
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div className="py-5">
-          <p>
-            Seja um apoiador do Curso em Vídeo, remova os anúncios do site,
-            tenha créditos para emissão de certificados e outros benefícios
-          </p>
-        </div>
-      </section>
+      <BannerPaginaCima dirname="Cursos"/>
       <section className="cards py-5">
         {
           data.map((json:{id: string, slug: string,image: string, title: string, description: string}) => (
