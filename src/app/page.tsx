b4f83f6d3e996fc96f2bea5d1437437b756cc8e1
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-05-04 12:06:24 -0300

    Deletando conteudo page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <header>
-        <nav>
-          <ul>
-            <li>HOme</li>
-            <li>Contatos</li>
-          </ul>
-        </nav>
-      </header>
-      <h1>algo demais</h1>
-      <main>
-        <form action="">
-          <input type="text" value="algo muito demais" />
-        </form>
-      </main>
-    </div>
+    <>
+      
+    </>
   );
 }
