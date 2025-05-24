776ad643f823bc4e1c3d1928e816c879ab54c950
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-05-24 12:55:55 -0300

    Retirando redimencinamento do flex, na parte circular do link video

--- src/app/components/menu_class_videos/LinkClassVideo.tsx
+++ src/app/components/menu_class_videos/LinkClassVideo.tsx
@@ -9,7 +9,7 @@ export interface PropsLinkClassVideo{
 export function LinkClassVideo({title, slug, course}:PropsLinkClassVideo){
     return (
         <div className="flex items-center gap-5 p-5 border-b-1 border-gray-300">
-            <span className="inline-block aling-middle size-7 border-3 border-gray-300 rounded-full">
+            <span className="shrink-0 size-7 border-3 border-gray-300 rounded-full">
                 {/* Parte circular */}
             </span>
             <Link href={`/cursos/${course}/${slug}`}>{title}</Link>
