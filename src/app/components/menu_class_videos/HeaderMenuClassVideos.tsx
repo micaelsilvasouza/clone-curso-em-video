fbdcb566b564a42a5e54c185705a511864a3df6b
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-05-24 13:17:47 -0300

    Criando lógica de abrir/fechar do menu de videos

--- src/app/components/menu_class_videos/HeaderMenuClassVideos.tsx
+++ src/app/components/menu_class_videos/HeaderMenuClassVideos.tsx
@@ -2,16 +2,22 @@ import { FaAngleDown, FaAngleUp } from "react-icons/fa"
 import { LuBook } from "react-icons/lu"
 
 interface PropsHeaderMenuClassVideos{
-    quantity: number
+    quantity: number,
+    isopening: boolean,
+    setIsOpening: (value: boolean)=>void
 }
 
-export default function HeaderMenuClassVideos({ quantity }:PropsHeaderMenuClassVideos){
+export default function HeaderMenuClassVideos({ quantity, isopening, setIsOpening }:PropsHeaderMenuClassVideos){
     return (
         <section className="flex flex-wrap gap-5 justify-between bg-blue-800 text-white text-lg font-bold p-10 rounded-t-xl relative">
             <p className="px-5"><LuBook className="inline align-middle"/> Conteudo</p>
             <p className="px-5">0% Completo | 0 / {quantity} steps</p>
-            <span className="absolute bottom-1 right-2">
-                <FaAngleDown /> <FaAngleUp />
+
+            {/* Alterando o estado do componente pai */}
+            <span className="absolute bottom-1 right-2" onClick={()=>{setIsOpening(!isopening)}}>
+                {
+                    isopening ? <FaAngleUp /> : <FaAngleDown />
+                }
             </span>
         </section>
     )
--- src/app/components/menu_class_videos/MenuClassVideos.tsx
+++ src/app/components/menu_class_videos/MenuClassVideos.tsx
@@ -1,3 +1,9 @@
+"use client"
+
+//hooks react
+import { useState } from "react"
+
+//componentes
 import { LinkClassVideo } from "./LinkClassVideo"
 import HeaderMenuClassVideos from "./HeaderMenuClassVideos"
 
@@ -9,10 +15,32 @@ interface PropsMenuClassVideos{
 }
 
 export default function MenuClassVideos({videos, course}: PropsMenuClassVideos){
+    const [isopening, setIsOpening] = useState(true)
+
     return (
         <section className="my-10 mx-auto w-9/10 max-w-[800px] border-2 border-gray-300 rounded-xl">
-            <HeaderMenuClassVideos quantity={videos.length}/>
-            <section className="px-10 py-5">
+            {/* Passando a alteraçaõ de estado para o componente filho */}
+            <HeaderMenuClassVideos quantity={videos.length} isopening={isopening} setIsOpening={setIsOpening}/>
+           
+            {
+            /*
+                Tentei fazer a transição de altura, mas não funcionou
+
+                tentei fazer usando o tailwind assim como está agora
+
+                tentei fazer com css normal passando dentro do style 
+                    [ style={{transitionDuration: ".3s"}} ]
+
+                tentei fazer também a alteração da altura dentro do style dentro do style
+                    [ style={{transitionDuration: ".3s"}, hieght: isopening ? "fit-content" : "0px"} } ]
+                
+                Em todos os caso a alterção de altura ocorre corretamente, mas a transição não acontece
+
+                pode ser bug no meu pc também
+            */
+            }
+
+            <section className={`duration-1000 px-10 overflow-hidden ${isopening ? "h-fit" : "h-0"}`} >
                 {videos.map((video: PropsVideoClass)=>(
                     <LinkClassVideo key={video.slug} title={video.title} slug={video.slug} course={course}/>
                 ))}
