a4009df053543115eec7f6d57f3c6a36c644b4f6
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-01 09:38:59 -0300

    Update MenuClassVideos.tsx

--- src/app/components/menu_class_videos/MenuClassVideos.tsx
+++ src/app/components/menu_class_videos/MenuClassVideos.tsx
@@ -52,7 +52,7 @@ export default function MenuClassVideos({type, videos, courseslug, coursetitle}:
             <section className={`
               relative 
               duration-800
-              min-h-[70dvh]
+              min-h-[100dvh]
               ${ isopening ? "w-[30dvw]" : "w-0"}
             `}>
               <section className={`flex flex-col absolute top-0 right-0 w-[30dvw] h-full bg-white`}>
