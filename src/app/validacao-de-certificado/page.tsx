f7f33f3c7076e557d45190df79e578adbeb0cefc
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-03 17:13:01 -0300

    Update page.tsx

--- src/app/validacao-de-certificado/page.tsx
+++ src/app/validacao-de-certificado/page.tsx
@@ -1,14 +1,15 @@
 import BannerRotaHeader from "../components/banner_rota_header";
 
-import { PiCertificateBold } from "react-icons/pi"
+import { PiCertificate } from "react-icons/pi"
 
 export default function ValidacaoDeCertificado(){
   return(
     <main>
       <BannerRotaHeader nomeRota="Validação de Certificado"/>
       <section className="text-center p-5">
-        <PiCertificateBold size={80} className="block mx-auto my-5" />
+        <PiCertificate size={80} className="block mx-auto my-5" />
         <p>Digite o código do certificado no formulário abaixo para verificar sua validade</p>
+        <input type="text" >
       </section>
     </main>
   )
