a7ba87369b5821601634185336cef349ab441035
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-04 13:30:24 -0300

    Delete src/app/components directory

--- src/app/components/layout/button/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./styles.module.css"
-
-interface ButtonProp{
-    title: string
-}
-
-export default function Button({title}: ButtonProp){
-    return <button className={styles.button_form} type="button">{title}</button>
-}
