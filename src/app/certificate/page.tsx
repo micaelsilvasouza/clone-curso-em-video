0b6085203520a63e47d0e8c13313119495d478ae
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-04 13:01:59 -0300

    Remove component layout

--- src/app/certificate/page.tsx
+++ src/app/certificate/page.tsx
@@ -1,8 +1,6 @@
 import CertificateLayout from "./layout"
 export default function Certificate(){
   return (
-    <CertificateLayout>
        <h1>Certificado de conclusão</h1>
-    </CertificateLayout>
   )
 }
