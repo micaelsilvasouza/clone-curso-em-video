83c62d8742c78ff2461cf7321c63d2c75561c044
Filipe520 <filipealves520@gmail.com>
2025-06-05 16:43:32 -0300

    update

--- src/app/apoie/page.tsx
+++ src/app/apoie/page.tsx
@@ -1,12 +1,44 @@
 import BannerRotaHeader from "../components/banner_rota_header";
 import SejaApoiador from "../components/seja-apoiador";
+import Img_custon from "../components/img_custon";
 
 import Link from "next/link";
-//Roboto amanhã colocar aqui
-import { Asap, Catamaran } from "next/font/google";
+
+import { Asap, Catamaran, Poppins } from "next/font/google";
 const catamaran = Catamaran({ subsets: ["latin"] });
-// const roboto = Roboto({ subsets: ["latin"] });
+const poppins = Poppins({ weight: ["800"] });
 const asap = Asap({ subsets: ["latin"] });
+
+const ArrayCourses = [
+  {
+    curso: "HTML5",
+    rota: "https://clone-curso-em-video.vercel.app/cursos/html5-css3-modulo1",
+  },
+  {
+    curso: "Java",
+    rota: "https://clone-curso-em-video.vercel.app/cursos/java-basico",
+  },
+  {
+    curso: "Word",
+    rota: "https://clone-curso-em-video.vercel.app/cursos/word",
+  },
+  {
+    curso: "Excel",
+    rota: "https://clone-curso-em-video.vercel.app/cursos/excel",
+  },
+  {
+    curso: "Marketing Digital",
+    rota: "https://clone-curso-em-video.vercel.app/cursos/marketing-digital",
+  },
+  {
+    curso: "Python",
+    rota: "https://clone-curso-em-video.vercel.app/cursos/python-3-mundo-1",
+  },
+  {
+    curso: "Hardware",
+    rota: "https://clone-curso-em-video.vercel.app/cursos/hardware",
+  },
+];
 
 export default function Apoie() {
   return (
@@ -28,51 +60,120 @@ export default function Apoie() {
           ></iframe>
         </section>
         <article className="md:px-5 px-3">
-          <h2 className={`text-5xl ${catamaran.className} font-bold`}>
-            E toda semana teremos{" "}
-            <span className="text-blue-700">conteúdos novos</span>.
+          <h2
+            className={`text-5xl lg:text-6xl ${catamaran.className} font-bold`}
+          >
+            E toda semana <br /> teremos{" "}
+            <span className="text-blue-700">
+              conteúdos <br /> novos
+            </span>
+            .
           </h2>
-          <p className={`${asap.className} leading-8 mt-20`}>
-            O{" "}
-            <Link
-              href="https://clone-curso-em-video.vercel.app/"
-              rel="internal"
-              className="text-blue-700 cursor-pointer px-1 hover:underline"
-            >
-              CursoemVideo
-            </Link>
-            surgiu em 2012 quando o professor{" "}
-            <Link
-              href={`https://www.instagram.com/gustavoguanabara/`}
-              target="_blank"
-              rel="external"
-              className="text-blue-700 cursor-pointer px-1 hover:underline"
-            >
-              Gustavo Guanabara
-            </Link>{" "}
-            decidiu transformar algumas de suas aulas em um formato de vídeo com
-            o objetivo de ajudar seus alunos em sala de aula. Em poucos meses, o
-            formato se mostrou bem popular e começou a chegar em outras pessoas
-            pelo mundo.
-          </p>
-          <p className={`${asap.className} leading-8 my-10`}>
-            Atualmente, são mais de 13 cursos grátis como Construção de Sites
-            com{" "}
-            <Link
-              href={`https://clone-curso-em-video.vercel.app/cursos/html5-css3-modulo1`}
-              target="_parent"
-              rel="internal"
-              className="text-blue-700 cursor-pointer px-1 hover:underline"
-            >
-              HTML5
-            </Link>
-            , Lógica de Programação, PHP, Java, Banco de Dados, Programação
-            Orientada a Objetos, Word, Excel, Marketing Digital, Python e
-            Hardware. E tudo com um único objetivo: democratizar a Informática e
-            provar que aprender programação ou qualquer outra disciplina técnica
-            não é tão difícil quanto as pessoas pensam.
-          </p>
+          <section className="md:flex gap-5 max-w-[900px] mx-auto">
+            <div>
+              <p
+                className={`${asap.className} leading-8 my-10 text-lg lg:text-xl`}
+              >
+                O{" "}
+                <Link
+                  href="https://clone-curso-em-video.vercel.app/"
+                  rel="internal"
+                  className="text-blue-700 cursor-pointer px-1 hover:underline"
+                >
+                  CursoemVideo
+                </Link>
+                surgiu em 2012 quando o professor{" "}
+                <Link
+                  href={`https://www.instagram.com/gustavoguanabara/`}
+                  target="_blank"
+                  rel="external"
+                  className="text-blue-700 cursor-pointer px-1 hover:underline"
+                >
+                  Gustavo Guanabara
+                </Link>{" "}
+                decidiu transformar algumas de suas aulas em um formato de vídeo
+                com o objetivo de ajudar seus alunos em sala de aula. Em poucos
+                meses, o formato se mostrou bem popular e começou a chegar em
+                outras pessoas pelo mundo.
+              </p>
+            </div>
+            <div>
+              <p
+                className={`${asap.className} leading-8 my-10 text-lg lg:text-xl`}
+              >
+                Atualmente, são mais de 13 cursos grátis como Construção de
+                Sites com
+                {ArrayCourses.map((curso, index) => (
+                  <Link
+                    href={curso.rota}
+                    key={index}
+                    className="text-blue-700 cursor-pointer px-1 hover:underline"
+                  >
+                    {`${curso.curso}, `}
+                  </Link>
+                ))}
+                . E tudo com um único objetivo: democratizar a Informática e
+                provar que aprender programação ou qualquer outra disciplina
+                técnica não é tão difícil quanto as pessoas pensam.
+              </p>
+            </div>
+          </section>
         </article>
+        <section className="xl:w-[1200px] md:w-[800px]">
+          <div>
+            <Img_custon
+              img="estudante.png"
+              alt="imagem do aluno curso em vídeo"
+              width={500}
+              className="mx-auto"
+            />
+          </div>
+
+          <section className="flex max-md:flex-col gap-3 mb-20 px-5 w-full">
+            <div className="w-full h-[500px] rounded-2xl bg-blue-700">
+              <div>
+                <h3
+                  className={`${asap.className} uppercase text-3xl text-white font-bold`}
+                >
+                  Apoio Mensal
+                </h3>
+                <h4
+                  className={`${poppins.className} text-5xl text-white font-bold`}
+                >
+                  R$ 29
+                </h4>
+                <p className="text-yellow-700">Mínimo de 6 meses de adesão.</p>
+                <p className="text-yellow-700">
+                  Só poderá ser cancelado após término de 6 meses.
+                </p>
+              </div>
+
+              <div>
+                <p>icone</p>
+                <h3>1 crédito</h3>
+                <h4>mensal para certificados</h4>
+              </div>
+              <div>
+                <p>icone</p>
+                <p>
+                  Sem anúncios. Navegue no Curso em Video sem anúncios no site
+                </p>
+                Presença no Hall da Fama <p>Nosso mais sincero agradecimento</p>{" "}
+                <p>
+                  Acesso à nossa Área de Recompensas, com vídeos no formato vlog
+                  exclusivos
+                </p>{" "}
+                <p>
+                  Aulas EXCLUSIVAS criadas apenas para meus alunos presenciais
+                  (Portugol Studio e Java)
+                </p>{" "}
+                <p>NÃO DÁ acesso ao Estudonauta Apoiar Agora!</p>
+              </div>
+            </div>
+            <div className="w-full h-[500px] rounded-2xl bg-red-600"></div>
+            <div className="w-full h-[500px] rounded-2xl bg-green-500"></div>
+          </section>
+        </section>
       </div>
     </div>
   );
