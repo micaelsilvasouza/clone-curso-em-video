26c3a1421d876cd582d5d2227e41180bb0100220
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-06 17:32:18 -0300

    Button print

--- src/app/validacao-de-certificado/page.tsx
+++ src/app/validacao-de-certificado/page.tsx
@@ -14,19 +14,17 @@ export default function ValidacaoDeCertificado(){
     }
   }
 
+  const printCerticate = () => {
+    if(iframe_ref.current){
+      iframe_ref.current.contentWindow.print()
+    }
+  }
+
 
   return(
     <main>
       <BannerRotaHeader nomeRota="Validação de Certificado"/>
       <section className="text-center p-5">
-        <div className="aspect-10/8 w-full max-w-[1000px]">
-          <iframe 
-            ref={iframe_ref}
-            src="https://certificate-exibition.netlify.app/?&code=exemple"
-            width="100%"
-            height="100%"
-          ></iframe>
-        </div>
         <p>Digite o código do certificado no formulário abaixo para verificar sua validade</p>
         <div className="flex flex-col gap-2">
               <label htmlFor="code" className="text-sm text-black/80">
@@ -50,6 +48,22 @@ export default function ValidacaoDeCertificado(){
                 Validar Certificado
               </button>
          </div>
+        <div className="aspect-10/8 w-full max-w-[1000px]">
+          <iframe 
+            ref={iframe_ref}
+            src="https://certificate-exibition.netlify.app/?&code=exemple"
+            width="100%"
+            height="100%"
+          ></iframe>
+        </div>
+        <div>
+           <button 
+                className="py-2 px-4 text-sm bg-blue-700 text-white rounded-lg"
+                onClick={printCerticate}
+              >
+                Imprimir Certificado
+              </button>
+        </div>
       </section>
     </main>
   )
