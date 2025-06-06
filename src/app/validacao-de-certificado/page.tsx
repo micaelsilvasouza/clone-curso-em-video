63e2ea23d7f78cc0c79ba2b7be7dc19d56239d53
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-06 17:14:28 -0300

    Add tipagem

--- src/app/validacao-de-certificado/page.tsx
+++ src/app/validacao-de-certificado/page.tsx
@@ -6,7 +6,7 @@ import { useState, useRef } from "react"
 
 export default function ValidacaoDeCertificado(){
   const [code, setCode] = useState("")
-  const iframe_ref = useRef<HTMLIframeElement>(null)
+  const iframe_ref = useRef<HTMLIFrameElement>(null)
 
   const validateCerticate = () => {
     alert(iframe_ref.current.textContent)
