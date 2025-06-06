93ad65dfbaafaa4a7b8c084e58a07a6b85cbeee7
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-05 22:37:48 -0300

    Update index.tsx

--- src/app/components/banner_course/index.tsx
+++ src/app/components/banner_course/index.tsx
@@ -1,4 +1,6 @@
 import Img_custon from "../img_custon";
+import Navbar from "@/app/components/layout/navbar";
+
 
 interface PropBannerCourse {
   title: string;
@@ -8,25 +10,32 @@ interface PropBannerCourse {
 
 export default function BannerCourse({ title, image, slug }: PropBannerCourse) {
   return (
-    <div className="grid justify-items-center p-8 pt-[120px] bg-blue-50 bg-[url(/image/BannerPaginaCima.jpg)] bg-cover bg-no-repeat bg-center md:justify-items-start md:grid-cols-4">
-      <Img_custon
-        className="size-[200px] md:size-50 md:row-span-2 rounded-full"
-        img={image}
-        alt={slug}
-        width={100}
+    <div className="bg-blue-50 bg-[url(/image/BannerPaginaCima.jpg)] bg-cover bg-no-repeat bg-center pt-5px">
+      <Navbar
+        logo="logoBlue.png"
+        styleLinks="text-black"
+        styleHamburguer="bg-black"
       />
-      <h1 className="pl-4 md:col-span-3">
-        <span className="block text-lg text-[#1c0051]">Curso de</span>
-        <span className="block text-4xl font-semibold text-[#1c0051]">
-          {title}
-        </span>
-      </h1>
-      <Img_custon
-        className="w-20 h-11"
-        img="https://filipe520.github.io/api-cursoEmVideo/img-cursoEmVideo/imagens%20aleatoria/gratis.png"
-        alt="Curso gratis"
-        width={100}
-      />
+      <div className="grid justify-items-center p-8 pt-[120px] md:justify-items-start md:grid-cols-4">
+        <Img_custon
+          className="size-[200px] md:size-50 md:row-span-2 rounded-full"
+          img={image}
+          alt={slug}
+          width={100}
+        />
+        <h1 className="pl-4 md:col-span-3">
+          <span className="block text-lg text-[#1c0051]">Curso de</span>
+          <span className="block text-4xl font-semibold text-[#1c0051]">
+            {title}
+          </span>
+        </h1>
+        <Img_custon
+          className="w-20 h-11"
+          img="https://filipe520.github.io/api-cursoEmVideo/img-cursoEmVideo/imagens%20aleatoria/gratis.png"
+          alt="Curso gratis"
+          width={100}
+        />
+      </div>
     </div>
   );
 }
