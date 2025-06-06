8e543f3ca659f9dee96bdf8c6a57837513b20fa6
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-06 10:41:54 -0300

    Criando pagina de error

---
+++ src/app/error/page.tsx
@@ -0,0 +1,15 @@
+import { type NextRequest } from "next/server"
+
+export default function ErrorPage(request: NextRequest){
+  const searchParams = request.nextUrl.searchParams
+  const error = searchParams.get("error")
+
+  console.log(error)
+  
+  return (
+    <>
+      <h1>Ocorreu um error:</h1>
+      <p>{error}</p>
+    </>
+  )
+}
