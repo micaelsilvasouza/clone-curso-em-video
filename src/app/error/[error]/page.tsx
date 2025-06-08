093638e5ed69f42bfbe6c1d0581166c662210cd3
Micael <micaelsouza22silva@gmail.com>
2025-06-08 16:03:09 +0000

    teste push do notebook

--- src/app/error/[error]/page.tsx
+++ src/app/error/[error]/page.tsx
@@ -8,6 +8,7 @@ export default async function ErrorPage({params,
     <>
       <h1>Ocorreu um error:</h1>
       <p>{error}</p>
+      <p>Retorne para a pagina inicial e tente novamente mais tarde</p>
     </>
   )
 }
