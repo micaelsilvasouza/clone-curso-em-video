0853752985457bb4e517b340e071aa8da4560faf
Micael <micaelsouza22silva@gmail.com>
2025-06-08 17:17:38 +0000

    LEROLERO

--- src/app/error/[error]/page.tsx
+++ src/app/error/[error]/page.tsx
@@ -6,6 +6,7 @@ export default async function ErrorPage({params,
   
   return (
     <>
+      <p>Corrreerffwfjlfkbjgjopjojvopnjopijgafjpjgoapsvjoajgopajajogjoagopagjapgjagpoj</p>
       <h1>Ocorreu um error:</h1>
       <p>{error}</p>
     </>
