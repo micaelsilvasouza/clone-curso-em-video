72cb53ab5260579ab105d7f0eba932599305d7f6
Micael <micaelsouza22silva@gmail.com>
2025-06-08 16:54:59 +0000

    mais uma vez

--- src/app/error/[error]/page.tsx
+++ src/app/error/[error]/page.tsx
@@ -8,9 +8,6 @@ export default async function ErrorPage({params,
     <>
       <h1>Ocorreu um error:</h1>
       <p>{error}</p>
-      <p>Retorne para a pagina inicial e tente novamente mais tarde</p>
-      <p>Descupe o incomodo</p>
-      <p>::--::</p>
     </>
   )
 }
