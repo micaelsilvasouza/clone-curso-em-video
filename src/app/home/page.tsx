21d1efeac38206b51961603c19ad8cee3f318c5d
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-13 18:44:26 -0300

    Update page.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,6 +3,7 @@ import { cookies } from "next/headers"
 
 export default async function Page(){
   const cookie = await cookies()
+  cookie.set("token", "TokeNoCool")
   return (
     <>
     <h1>Usando cookies</h1>
