69dadced64cafef39d249d9ad9d03aa68099ce47
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-13 11:23:51 -0300

    Update index.tsx

--- src/app/components/patrocine_text_box/index.tsx
+++ src/app/components/patrocine_text_box/index.tsx
@@ -6,10 +6,10 @@ interface PropsTextBox{
 
 export default function TextBox({Icon, title, paragraph}: PropsTextBox){
   return (
-    <div>
-      <Icon />
-      <h1>{title}</h1>
-      <p>{paragraph}</p>
+    <div className="bg-white rounded-xl p-5 m-2 shadow-lg">
+      <Icon className="text-4xl text-[#2727ff] m-2"/>
+      <h1 className="text-2xl font-semibold p-2">{title}</h1>
+      <p className="text-lg p-2">{paragraph}</p>
     </div>
   )
 }
