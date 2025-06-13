ed96f643b0fdd857961a6883f5fcbf57914dab81
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-13 18:56:51 -0300

    Update

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,16 @@
- import { cookies } from "next/headers"
+import { cookies } from "next/headers"
 
 
 export default async function Page(){
   const cookie = await cookies()
+  
   cookie.set("token", "TokeNoCool")
+
+  const a = cookie.has("token")
   return (
     <>
     <h1>Usando cookies</h1>
-    <p>{cookie.has("token") ? "token" : cookie.get("token")}</p>
+    <p>{a}</p>
     </>
   )
 }
