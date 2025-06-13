d65a7281c396cf558639aee6e3db54710f586cab
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-13 09:34:19 -0300

    update layout

--- src/app/seja-um-patrocinador/page.tsx
+++ src/app/seja-um-patrocinador/page.tsx
@@ -4,14 +4,12 @@ import Img_custon from "../components/img_custon";
 import Navbar from "../components/layout/navbar";
 import CircleLoddingNumber from "../components/circle_lodding_numbers/";
 
-import { FaYoutube } from "react-icons/fa";
+import { FaYoutube, FaInstagram, FaTiktok } from "react-icons/fa";
 import { BiWorld } from "react-icons/bi";
-
-
 
 export default function Apoie() {
   return (
-    <main>
+    <main className="bg-gray-200">
       <section className="flex flex-col h-full bg-[url('https://filipe520.github.io/api-cursoEmVideo/img-cursoEmVideo/pagina_home/fundo08.jpg')] bg-center bg-cover bg-no-repeat px-6 pt-10">
         <Navbar
           logo="logoWhite.png"
@@ -41,7 +39,7 @@ export default function Apoie() {
           />
         </div>
       </section>  
-      <section className="w-full max-w-[1000px] m-auto">
+      <section className="bg-white w-full max-w-[1000px] m-auto">
         <h1 className="text-center text-3xl/10 lg:text-5xl/15 text-sky-400 font-bold p-10">Audiência segmentada na área de tecnologia da informação</h1>
         <p className="text-center text-xl lg:text-3xl px-10">O Curso em Vídeo é um dos maiores e mais reconhecidos projetos de educação em tecnologia no Brasil.</p>
         <div className="pb-5">
@@ -54,8 +52,13 @@ export default function Apoie() {
         </div>
       </section>
       <section>
-        <CircleLoddingNumber name="Youtube" Icon={FaYoutube} total={2.5} increment={0.1}/>
-        <CircleLoddingNumber name="Site" Icon={BiWorld} total={1} increment={1}/>
+        <h1 className="text-center text-3xl lg:text-5xl font-bold p-10">Audiência do Curso em Video</h1>
+        <div className="flex flex-wrap justify-center items-center p-10">
+          <CircleLoddingNumber name="Youtube" Icon={FaYoutube} total={2.5} increment={0.1}/>
+          <CircleLoddingNumber name="Site" Icon={BiWorld} total={1} increment={0.1}/>
+          <CircleLoddingNumber name="Instagram" Icon={FaInstagram} total={217} increment={1}/>
+          <CircleLoddingNumber name="Tiktok" Icon={FaTiktok } total={57} increment={1}/>
+        </div>
       </section>
     </main>
   );
