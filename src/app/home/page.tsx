57ff84a9be5086a30413a7c97273a652d613ff6d
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-13 18:25:54 -0300

    Update page.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,12 @@
+import { cookies } from "next/headers"
+
+
 export default function Page(){
+  const cookie = await cookies()
   return (
-    <h1>Página na home do seu coração</h1>
+    <>
+    <h1>Usando cookies</h1>
+    <p>{cookie.get("token")}</p>
+    </>
   )
 }
