59f69c89b4e589e63ac1183b666617bf36965423
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-13 22:39:46 -0300

    Update page.tsx

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,15 +3,17 @@ import { cookies } from "next/headers"
 
 export default async function Page(){
   const cookie = await cookies()
-  
-  cookie.set("token", "TokeNoCool")
+
+  const save = ()=>{
+     cookie.set("token", "TokeNoCool")
+  }
 
   const a = cookie.has("token")
   return (
     <>
     <h1>Usando cookies</h1>
     <p>{a ? "token" : "kionko"}</p>
-    <button>Save</button>
+    <button onClick={save}>Save</button>
     </>
   )
 }
