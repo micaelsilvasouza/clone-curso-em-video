1c5e7fe89795406e3932977f50a7b2b4908711a9
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-15 18:06:35 -0300

    Update page.tsx

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,18 +2,13 @@
 
 
 export default async function Page(){
- // const cookie = await cookies()
-
-  const save = ()=>{
-     alert("TokeNoCool")
-  }
 
   const a = false //cookie.has("token")
   return (
     <>
     <h1>Usando cookies</h1>
     <p>{a ? "token" : "kionko"}</p>
-    <button formAction={save}>Save</button>
+    <button>Save</button>
     </>
   )
 }
