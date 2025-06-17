cde63f1222e7b9bd700349c114d3c7ff93ab8caf
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-17 09:23:51 -0300

    Update page.tsx

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,5 @@
 import {cookies} from "next/headers"
-import Btn from "@/componentes/Btn"
+import Btn from "../../componentes/Btn"
 
 export default async function Page(){
   const cookie = await cookies()
