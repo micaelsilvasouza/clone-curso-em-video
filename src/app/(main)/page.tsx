942c757fc79dc8b77d3493843b017fa9085ded15
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-17 09:21:09 -0300

    Update page.tsx

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,15 @@
 import {cookies} from "next/headers"
+import Btn from "@/componentes/Btn"
 
 export default async function Page(){
   const cookie = await cookies()
 
   const a = cookie.has("token")
 
-  return <h1>{a ? "TRUE" : "FALSE"}</h1>
+  return (
+    <>
+      <h1>{a ? "TRUE" : "FALSE"}</h1>
+      <Btn/>
+    </>
+)
 }
