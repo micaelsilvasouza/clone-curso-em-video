95254840697bc889e95fdaf38a367b76b43f55d8
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-17 22:05:09 -0300

    Update page.tsx

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,7 +3,7 @@ import Btn from "@/componetes/Btn"
 
 export default async function Page(){
   const cookie = await cookies()
-
+  console.log("getting")
   const a = cookie.has("token")
   const b = cookie.getAll()
 
