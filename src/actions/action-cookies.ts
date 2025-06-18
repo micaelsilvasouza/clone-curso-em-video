86ff4c58f154adc180a2944225bfb0ce176d084f
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-17 21:48:09 -0300

    Update action-cookies.ts

--- src/actions/action-cookies.ts
+++ src/actions/action-cookies.ts
@@ -1,10 +1,10 @@
 "use server"
 import {cookies} from "next/headers"
-import {revalidatePath} from "next/cache"
+import {redirect} from "next/navigation"
 
 export async function saveCookie(){
   const cookie = await cookies()
 
   cookie.set("token", "cookies")
-  revalidatePath("/")
+  redirect("/test")
 }
