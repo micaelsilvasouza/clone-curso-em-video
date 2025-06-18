4626de227fd75ce769f3f80268cecb06af7490a6
Micael Silva Souza <108833081+micaelsilvasouza@users.noreply.github.com>
2025-06-17 21:10:36 -0300

    Import saveCookie

--- src/componetes/Btn.tsx
+++ src/componetes/Btn.tsx
@@ -1,5 +1,6 @@
 "use client"
+import {saveCookie} from "@/actions/action-cookies"
 
 export default function Btn(){
-  return <button formAction={()=>alert("flocos")}>Click em min</button>
+  return <button formAction={saveCookie}>Click em min</button>
 }
