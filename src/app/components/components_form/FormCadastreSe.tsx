3afe00f4d39c9b91794af4fd043d656a70f01752
Filipe520 <filipealves520@gmail.com>
2025-06-28 23:58:37 -0300

    update

--- src/app/components/components_form/FormCadastreSe.tsx
+++ src/app/components/components_form/FormCadastreSe.tsx
@@ -13,6 +13,7 @@ import Link from "next/link";
 import LoadingCircleSpinner from "../search_custon/LoadingCircleSpinner";
 
 export default function FormCadastreSe({ stylesForm }: { stylesForm: string }) {
+  const [animationBtn, setAnimationBtn] = useState(false);
   const [name, setNome] = useState("");
   const [lastname, setLastNome] = useState("");
   const [email, setEmail] = useState("");
@@ -59,33 +60,41 @@ export default function FormCadastreSe({ stylesForm }: { stylesForm: string }) {
     }
 
     if (notification === false) {
-      setIslodding(true)
-      fetch("https://backend-cursoemvideo.onrender.com/user/register",{
+      setIslodding(true);
+      fetch("https://backend-cursoemvideo.onrender.com/user/register", {
         method: "post",
-        headers: {"Content-type": "application/json"},
+        headers: { "Content-type": "application/json" },
         body: JSON.stringify({
           name: name + " " + lastname,
           email: email,
-          password: password
-        })
+          password: password,
+        }),
       })
         .then((res) => res.json())
         .then((data) => {
-          setIslodding(false)
+          setIslodding(false);
           //caso ocorra erro no cadastramento
-          if(data.error){
+          if (data.error) {
             setMessageType("erro");
-          }else{
-            setMessageType("sucesso");
-            router.push("/login")
+          } else {
+            // Ativa a ANIMAÇÃO do botão cadastrar
+            setAnimationBtn(true);
+            if (data.message) {
+              // vai esperar 3 segundo para efetua o cadastro
+              setTimeout(() => {
+                setMessageType("sucesso");
+                router.push("/login");
+              }, 3000);
+            } else {
+              return;
+            }
           }
-          
+
           setNotification(true);
           setMessage(data.message);
-
         })
         .catch(() => {
-          setIslodding(false)
+          setIslodding(false);
           setNotification(true);
           setMessage("Ocorreu um erro ao buscar os aluno");
           setMessageType("erro");
@@ -95,74 +104,79 @@ export default function FormCadastreSe({ stylesForm }: { stylesForm: string }) {
 
   return (
     <>
-    <div className={`bg-[#00000035] fixed top-0 left-0 w-full h-full z-100 ${islodding?"flex":"hidden"} items-center justify-center`}>
-      <LoadingCircleSpinner/> 
-    </div>
-    <form className={stylesForm}>
-      <NotificacaoFlutuante
-        mensagem={message}
-        tipo={messageType}
-        ativo={notification}
-        setAtivo={setNotification}
-      />
-      <InputForm
-        name="firstname"
-        label="Nome *"
-        placeholder="Digite o nome que usará no certificado"
-        type="text"
-        changeFunction={setNome}
-      />
-      <InputForm
-        name="lastname"
-        label="Sobrenome *"
-        placeholder="Digite o sobrenome que usará no certificado"
-        type="text"
-        changeFunction={setLastNome}
-      />
-      <InputForm
-        name="email"
-        label="E-mail *"
-        placeholder="Digite seu melhor e-mail..."
-        type="email"
-        changeFunction={setEmail}
-      />
-      <InputForm
-        name="confirm-email"
-        label="Confirme seu e-mail *"
-        placeholder="Confirme seu e-mail..."
-        type="email"
-        changeFunction={setCEmail}
-      />
-      <InputForm
-        name="passoword"
-        label="Senha *"
-        placeholder="Digite a senha."
-        type="password"
-        changeFunction={setPassword}
-      />
-      <InputForm
-        name="confirm-password"
-        label="Confirme a senha. *"
-        placeholder="Digite novamente sua senha."
-        type="password"
-        changeFunction={setCPassword}
-      />
-      <div className="flex flex-col">
-        <Button
-          title="Registrar Gratuitamente"
-          styles="bg-blue-800 text-white py-4 mt-3"
-          clickFunction={validate}
-        />
-        <span className="mx-auto">
-          <Link
-            href={"/login"}
-            className="text-blue-700 pl-2 underline active:opacity-50"
-          >
-            Acesse sua conta
-          </Link>
-        </span>
+      <div
+        className={`bg-[#00000035] fixed top-0 left-0 w-full h-full z-100 ${
+          islodding ? "flex" : "hidden"
+        } items-center justify-center`}
+      >
+        <LoadingCircleSpinner />
       </div>
-    </form>
+      <form className={stylesForm}>
+        <NotificacaoFlutuante
+          mensagem={message}
+          tipo={messageType}
+          ativo={notification}
+          setAtivo={setNotification}
+        />
+        <InputForm
+          name="firstname"
+          label="Nome *"
+          placeholder="Digite o nome que usará no certificado"
+          type="text"
+          changeFunction={setNome}
+        />
+        <InputForm
+          name="lastname"
+          label="Sobrenome *"
+          placeholder="Digite o sobrenome que usará no certificado"
+          type="text"
+          changeFunction={setLastNome}
+        />
+        <InputForm
+          name="email"
+          label="E-mail *"
+          placeholder="Digite seu melhor e-mail..."
+          type="email"
+          changeFunction={setEmail}
+        />
+        <InputForm
+          name="confirm-email"
+          label="Confirme seu e-mail *"
+          placeholder="Confirme seu e-mail..."
+          type="email"
+          changeFunction={setCEmail}
+        />
+        <InputForm
+          name="passoword"
+          label="Senha *"
+          placeholder="Digite a senha."
+          type="password"
+          changeFunction={setPassword}
+        />
+        <InputForm
+          name="confirm-password"
+          label="Confirme a senha. *"
+          placeholder="Digite novamente sua senha."
+          type="password"
+          changeFunction={setCPassword}
+        />
+        <div className="flex flex-col">
+          <Button
+            title="Registrar Gratuitamente"
+            styles="bg-blue-800 text-white py-4 mt-3"
+            clickFunction={validate}
+            animation={animationBtn}
+          />
+          <span className="mx-auto">
+            <Link
+              href={"/login"}
+              className="text-blue-700 pl-2 underline active:opacity-50"
+            >
+              Acesse sua conta
+            </Link>
+          </span>
+        </div>
+      </form>
     </>
   );
 }
